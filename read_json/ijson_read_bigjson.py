67815a0fe72343c654d02348727d929ef738a79d
leixyou <leixyou@gmail.com>
2018-05-14 14:53:16 +0800

    Update ijson_read_bigjson.py
    使用该脚本处理了6个G日志

--- read_json/ijson_read_bigjson.py
+++ read_json/ijson_read_bigjson.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python
 import ijson
 import json
+
 def read_apistats(report_filepath):
     
     with open(report_filepath,'r') as f:
-        apistats=list(ijson.items(f,'behavior.apistats'))
-        #print apistats
+        apistats=ijson.items(f,'behavior.apistats')
+        apis=[]
+        for pid in apistats:
+            for api in pid.values():
+                apis.append(api)
         f.close()
-    return apistats
+    return apis
 
-def write_file(filepath,data):
-    f=open(filepath,'a+')
-    f.truncate()
-    if type(data) is not str:
-        #for x in data:
-            #f.write(x)
-            #f.write('\n')
-        f.write(str(data))
-    else:
-        f.write(data)
-    f.close()
-    return 
+def write_file(filepath,apis):
+    with open(filepath,'a+') as f:
+        f.truncate()
+        for x in apis:
+            f.write(json.dumps(x))
+            f.write('\n')
+        f.close()
 if __name__=='__main__':
     report_filepath="./report.json"
-    apistats=read_apistats(report_filepath)
-    write_file('./result.json',apistats)
+    apis=read_apistats(report_filepath)
+
+    write_file('./result.json',apis)

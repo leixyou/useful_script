6a0d94b88838ff5ecce7d31715b038b59a9b3ac5
leixyou <leixyou@gmail.com>
2018-05-16 16:43:54 +0800

    Update ijson_read_bigjson.py

--- read_json/ijson_read_bigjson.py
+++ read_json/ijson_read_bigjson.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import ijson
 import json
-
+import optparse
 def read_apistats(report_filepath):
     
     with open(report_filepath,'r') as f:
@@ -21,7 +21,14 @@ def write_file(filepath,apis):
             f.write('\n')
         f.close()
 if __name__=='__main__':
-    report_filepath="./report.json"
-    apis=read_apistats(report_filepath)
-
-    write_file('./result.json',apis)
+    option=optparse.OptionParser(usage='usage: %prog [src_report.json] [dest_filname.json]\n' )
+    (options,args)=option.parse_args()
+    
+    if len(args)!=2:
+        print option.print_usage()
+    else:
+        report_filepath=args[0]
+        apis=read_apistats(report_filepath)
+    
+        write_file(args[1],apis)
+    

67932e11899c508c0f24b0559f008fa86b54eaba
leixyou <leixyou@gmail.com>
2018-05-23 02:43:01 +0800

    fix bug of json serializable for decimal type

--- read_json/ijson_read_bigjson.py
+++ read_json/ijson_read_bigjson.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 import ijson
 import json
-import optparse
+import getopt
+import sys
+import decimal
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return float(o)
+        super(DecimalEncoder, self).default(o)
+
+
+
 def read_apistats(report_filepath):
     
     with open(report_filepath,'r') as f:
@@ -19,25 +29,39 @@ def read_dlls(report_filepath):
 def write_file(filepath,apis):
     with open(filepath,'a+') as f:
         f.truncate()
-        f.write(json.dumps(apis))
+        f.write(json.dumps(apis,cls=DecimalEncoder))
         #for x in apis:
             #f.write(json.dumps(x))
             #f.write('\n')
         f.close()
+        
+        
+#----------------------------------------------------------------------
+def usage():
+    """"""
+    print '-h --help print usage'
+    print '-f --file=filepath '
+    print '-d --read_dlls to read dlls'
+        
+        
 if __name__=='__main__':
-    option=optparse.OptionParser(usage='usage: %prog [src_report.json] [dest_filname.json]\n' )
-    option_list=['read_apistats','read_dlls']
-    option.add_options(option_list)
-    (options,args)=option.parse_args()
-    
-    
-    print option.print_usage()
- 
-    report_filepath='./report.json'
-    #apis=read_apistats(report_filepath)
-    generic=read_dlls(report_filepath)
-    #write_file(args[1],apis)
     try:
-        write_file(args[1], generic)
+        options,args=getopt.getopt(sys.argv[1:],"hadf:",['help','read_apistats','read_dll','file='])
     except:
-        print 'no second recv file'
+        sys.exit()
+        
+        
+    for op,args in options:
+        if op in ("-h",'--help'):
+            usage()
+            
+        if op in ('-f','--file'):
+            report_filepath=args
+            if report_filepath=='':
+                usage()
+        if op in ('-a','--read_apistats'):
+            apis=read_apistats(report_filepath)
+            write_file('./apistats.json', apis)
+        if op in ('-d','--read_dlls'):
+            dlls=read_dlls(report_filepath)
+            write_file('./dlls.json',dlls)

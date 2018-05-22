675ec5b2132e7a8a5337dd5cf07870ba2a2e202a
leixyou <leixyou@gmail.com>
2018-05-23 02:42:09 +0800

    fix bug of json serializable for decimal type

--- read_json/ijson_read_bigjson.py
+++ read_json/ijson_read_bigjson.py
@@ -6,14 +6,16 @@ def read_apistats(report_filepath):
     
     with open(report_filepath,'r') as f:
         apistats=list(ijson.items(f,'behavior.apistats'))
-        #apis=[]
-        #for pid in apistats:
-            #for api in pid.values():
-                #apis.append(api)
-        #f.close()
-        #
+
     return apistats
 
+#----------------------------------------------------------------------
+def read_dlls(report_filepath):
+    """show loaded dlls"""
+    with open(report_filepath,'r') as f:
+        generic=list(ijson.items(f,'behavior.generic'))
+        f.close()
+    return generic
 def write_file(filepath,apis):
     with open(filepath,'a+') as f:
         f.truncate()
@@ -24,13 +26,18 @@ def write_file(filepath,apis):
         f.close()
 if __name__=='__main__':
     option=optparse.OptionParser(usage='usage: %prog [src_report.json] [dest_filname.json]\n' )
+    option_list=['read_apistats','read_dlls']
+    option.add_options(option_list)
     (options,args)=option.parse_args()
     
-    if len(args)!=2:
-        print option.print_usage()
-    else:
-        report_filepath=args[0]
-        apis=read_apistats(report_filepath)
     
-        write_file(args[1],apis)
-    
+    print option.print_usage()
+ 
+    report_filepath='./report.json'
+    #apis=read_apistats(report_filepath)
+    generic=read_dlls(report_filepath)
+    #write_file(args[1],apis)
+    try:
+        write_file(args[1], generic)
+    except:
+        print 'no second recv file'
